9a2956268e7a7a1c6ab6b36829c7f1cd9e358617
Luuk-Gunnewijk <115221003+Luuk-Gunnewijk@users.noreply.github.com>
2026-03-25 12:56:13 +0100

    Added an OpenLevel Function

--- Co-op Game/Assets/Scripts/Mangers/LevelManger.cs
+++ Co-op Game/Assets/Scripts/Mangers/LevelManger.cs
@@ -1,10 +1,15 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManger : MonoBehaviour
 {
+    [Header("UI Elements")]
     [SerializeField] private GameObject mainMenuUI;
     [SerializeField] private GameObject levelSelectionUI;
+
+    [Header("All Levels")]
+    [SerializeField] private string[] levels;
 
     private void Start()
     {
@@ -31,5 +36,11 @@ public class LevelManger : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void OpenLevel(int index)
+    {
+        if (index < 0 || index >= levels.Length) { return; }
+        SceneManager.LoadScene(levels[index]);
     }
 }
