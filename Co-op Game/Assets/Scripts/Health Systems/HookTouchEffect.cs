d63a6b39153d06c1f98b3fae6be794cd64682721
Luuk-Gunnewijk <115221003+Luuk-Gunnewijk@users.noreply.github.com>
2026-03-26 15:04:12 +0100

    Start of Hook Touch Effect

---
+++ Co-op Game/Assets/Scripts/Health Systems/HookTouchEffect.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HookTouchEffect : MonoBehaviour
+{
+    [Header("Scripts")]
+    [SerializeField] private PlayersHealth playersHealth;
+
+    [Header("Effects")]
+    [SerializeField] private ParticleSystem hitEffect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Instantiate(hitEffect, transform.position, Quaternion.identity);
+            playersHealth.TakingDamage();
+        }
+    }
+}
