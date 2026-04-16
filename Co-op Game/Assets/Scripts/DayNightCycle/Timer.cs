0356422851a008980c5f05b7a67e7b380c634ee5
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2026-04-16 10:06:45 +0200

    Timer created

---
+++ Co-op Game/Assets/Scripts/DayNightCycle/Timer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Change Cycle settings")]
+    [Tooltip("The Seconds how long this timer is.")][SerializeField] private float cycleDuration;
+
+    [SerializeField] private Image filledImage;
+    private float _timer;
+    private int count = 0;
+
+    private void Start()
+    {
+        _timer = cycleDuration;
+        StartCoroutine(UpdateScroller());
+    }
+
+    private IEnumerator UpdateScroller()
+    {
+        yield return new WaitForSeconds(0);
+        _timer -= Time.deltaTime;
+        Debug.Log(_timer);
+        if (_timer < 0)
+        {
+            StopCoroutine(UpdateScroller());
+            WinEvent.TriggerWin();
+        }
+
+        if (!(_timer > 0)) yield break;
+        var currentTime = _timer / cycleDuration;
+        filledImage.fillAmount = currentTime;
+        yield return StartCoroutine(UpdateScroller());
+
+    }
+}
