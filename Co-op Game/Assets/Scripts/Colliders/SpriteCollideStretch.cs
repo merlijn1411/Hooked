ff8423d1884f9fd7512cc3e310de38aa06f1f219
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2026-04-17 15:11:49 +0200

    Sack completed

--- Co-op Game/Assets/Scripts/Colliders/SpriteCollideStretch.cs
+++ Co-op Game/Assets/Scripts/Colliders/SpriteCollideStretch.cs
@@ -10,16 +10,16 @@ public class SpriteCollideStretch : MonoBehaviour
     [SerializeField] private float returnSpeed = 5f;
 
     private Transform[] _bones;
-    private Vector3[] defaultLocalPositions;
+    private Vector3[] _defaultLocalPositions;
     
     private void Start()
     {
         _bones = spriteSkin.boneTransforms;
-        defaultLocalPositions = new Vector3[_bones.Length];
+        _defaultLocalPositions = new Vector3[_bones.Length];
 
         for (var i = 0; i < _bones.Length; i++)
         {
-            defaultLocalPositions[i] = _bones[i].localPosition;
+            _defaultLocalPositions[i] = _bones[i].localPosition;
         }
     }
 
@@ -60,7 +60,7 @@ public class SpriteCollideStretch : MonoBehaviour
         {
             _bones[i].localPosition = Vector3.Lerp(
                 _bones[i].localPosition,
-                defaultLocalPositions[i],
+                _defaultLocalPositions[i],
                 Time.deltaTime * returnSpeed
             );
         }
