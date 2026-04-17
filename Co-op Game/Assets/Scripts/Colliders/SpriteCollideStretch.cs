d0e9672179e9983b16b533da4ff391ae78951434
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2026-04-17 12:26:33 +0200

    Stretch for sack implemented and transform changes in sack
    Changes:
    - zak.prefab
    Add:
    - SpriteCollideSTretch.cs

---
+++ Co-op Game/Assets/Scripts/Colliders/SpriteCollideStretch.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+public class SpriteCollideStretch : MonoBehaviour
+{
+    [SerializeField] private SpriteSkin spriteSkin;
+    
+    [SerializeField] private float stretchStrength = 0.2f;
+    [SerializeField] private float influenceRadius = 1.0f;
+    [SerializeField] private float returnSpeed = 5f;
+
+    private Transform[] _bones;
+    private Vector3[] defaultLocalPositions;
+    
+    private void Start()
+    {
+        _bones = spriteSkin.boneTransforms;
+        defaultLocalPositions = new Vector3[_bones.Length];
+
+        for (var i = 0; i < _bones.Length; i++)
+        {
+            defaultLocalPositions[i] = _bones[i].localPosition;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        foreach (var contact in collision.contacts)
+        {
+            ApplyStretch(contact.point);
+        }
+    }
+
+    private void ApplyStretch(Vector2 hitPoint)
+    {
+        for (var i = 0; i < _bones.Length; i++)
+        {
+            var bone = _bones[i];
+
+            var dist = Vector2.Distance(bone.position, hitPoint);
+
+            if (dist < influenceRadius)
+            {
+                var falloff = 1f - (dist / influenceRadius);
+                falloff *= falloff;
+                falloff = Mathf.Clamp01(falloff);
+                                
+                var dir = (bone.position - (Vector3)hitPoint).normalized;
+
+                var offset = (dir * stretchStrength * falloff);
+
+                bone.localPosition += offset;
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        for (var i = 0; i < _bones.Length; i++)
+        {
+            _bones[i].localPosition = Vector3.Lerp(
+                _bones[i].localPosition,
+                defaultLocalPositions[i],
+                Time.deltaTime * returnSpeed
+            );
+        }
+    }
+}
