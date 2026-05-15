8e1febcfae16eefc44f765d48ccaf5688ce4b9f6
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2026-05-15 10:53:03 +0200

    Update SceneTransitioner.cs

--- Co-op Game/Assets/Scripts/Mangers/SceneTransitioner.cs
+++ Co-op Game/Assets/Scripts/Mangers/SceneTransitioner.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class SceneTransitioner : MonoBehaviour
 {
-    [SerializeField] private List<Animator> animators;
+    [SerializeField] private List<GameObject> parentHolders;
+    private List<Animator> _animators = new List<Animator>();
 
     private void Start()
     {
-        PlayTransition();
+        InitializeAnimators();
+    }
+    
+    private void InitializeAnimators()
+    {
+        foreach (var holder in parentHolders)
+        {
+            var childObjects = holder.GetComponentsInChildren<Animator>();
+            foreach (var childObject in childObjects)
+            {
+                _animators.Add(childObject);
+            }
+        }
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayTransition();
+        }
+        
+    }
 
     private void PlayTransition()
     {
-        foreach (var animator in animators)
+        foreach (var animator in _animators)
         {
            animator.SetTrigger("Play");
         }
